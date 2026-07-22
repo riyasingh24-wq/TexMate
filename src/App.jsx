eefc6a03403d385f3d936564a550966a4d2b0e72
Riya <Riya@riyasingh.com>
2026-07-22 15:57:57 +0530

    chore: configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-
-import { House } from "lucide-react";
-
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
-      <House size={40} />
-      <h1 className="text-3xl font-bold text-blue-600 mt-4">
-        Tailwind + Lucide Working 🚀
-      </h1>
-    </div>
+    <>
+      <h1>My Project</h1>
+    </>
   );
 }
 
